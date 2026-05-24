15d561e2a578d4417bbf28cb15498817ebe7d2fe
Anauee <anaue736@gmail.com>
2026-05-24 13:53:27 -0300

    feat: instalar pixel oficial da Meta (ID: 863124242826998) para fbp e fbc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Utmify Pixel */}
         <Script id="utmify-pixel" strategy="afterInteractive">
           {`
             window.pixelId = "6a0f5105a7e5556380e2dee4";
@@ -26,8 +27,34 @@ export default function RootLayout({
             document.head.appendChild(a);
           `}
         </Script>
+
+        {/* Meta Pixel (Facebook Pixel) */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '863124242826998');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body>
+        {/* Facebook Pixel Fallback for browsers without JS */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=863124242826998&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         <WhatsAppButton />
       </body>
